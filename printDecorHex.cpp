6e6a4e8fd78cd2da7b6aa306b602a8ffb690aa5a
comditon <comditon@gmail.com>
2018-07-12 22:20:33 +0900

    comment and small change

--- printDecorHex.cpp
+++ printDecorHex.cpp
@@ -1,3 +1,5 @@
+/* integers can be printed as a hexadecimal number or a decimal number */
+
 #include <iostream>
 
 using namespace std;
@@ -6,8 +8,8 @@ int main(void){
   int a = 12121;
   int b = 0xabab;
 
-  cout << hex << "in hex, a = " << a << dec << "\t\t in dec, a = " << a << endl;
-  cout << hex << "in hex, b = " << b << dec << "\t\t in dec, b = " << b << endl;
+  cout << hex << "in hex, a = " << a << dec << "\t in dec, a = " << a << endl;
+  cout << hex << "in hex, b = " << b << dec << "\t in dec, b = " << b << endl;
 
   return 0;
 }
